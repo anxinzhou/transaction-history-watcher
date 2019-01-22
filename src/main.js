96309c68a1a7ab0cc11ff901c99f25b776f53b2b
anxin <anxin@anxins-MacBook-Pro.local>
2019-01-22 23:36:06 +0800

    clean unimportant file

--- src/main.js
+++ src/main.js
@@ -1,45 +1,6 @@
-const Database = require('./database');
-const Contract = require('./contract');
+getTranctionHistory = require('./transactionHistory');
 
-const contract = new Contract("../etc/contractConfig.json");
-const db = new Database("../etc/dbConfig.json");
-db.createIndex("from").catch(console.log);
-// db.find({},'limit(1)')).then(console.log).catch(console.log)
-let checkpoint = {
-    get: function() {
-        return db.getCheckPoint().then(obj=>obj["checkpoint"])
-    },
-    set: function (newVal) {
-        return db.setCheckPoint(newVal)
-    }
-};
-
-// checkpoint is default to be db.initialCheckpoint at the beginning
-// will start from checkpoint, unless initial=true,
-// when initial = true, checkpoint will be set to db.initialCheckpoint
-startListening(1000,inital = false);
-
-
-async function startListening(interval,initial = false) {
-    if(initial) {
-        await checkpoint.set(db.initialCheckpoint);
-    }
-    while(1) {
-        try {
-            await contract.getHistoryTxFromCheckPoint(checkpoint,callback);
-        } catch (err) {
-            console.log(err.message);
-        }
-        await sleep(interval)
-    }
-}
-
-function callback(txs) {
-    if(txs.length!==0) {
-        return Database.prototype.insertMany.bind(db)(txs);
-    }
-}
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+// walletId, page, offset
+getTranctionHistory("0x88d6F234b6327b7a9d9d23014EFd6E05b6Dc8943",1,10)
+    .then(console.log)
+    .catch(console.log);
--- src/test.js
+++
@@ -1,6 +0,0 @@
-getTranctionHistory = require('./transactionHistory');
-
-// walletId, page, offset
-getTranctionHistory("0x88d6F234b6327b7a9d9d23014EFd6E05b6Dc8943",1,10)
-    .then(console.log)
-    .catch(console.log);
