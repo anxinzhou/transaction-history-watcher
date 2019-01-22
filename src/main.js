21b427b79e3ba08502213097162d250030d03d92
anxin <anxin@anxins-MacBook-Pro.local>
2019-01-22 23:39:30 +0800

    api key config

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 getTranctionHistory = require('./transactionHistory');
 
-// walletId, page, offset
+// paras: walletId, page, offset
+// return: promise txs array
 getTranctionHistory("0x88d6F234b6327b7a9d9d23014EFd6E05b6Dc8943",1,10)
     .then(console.log)
     .catch(console.log);
