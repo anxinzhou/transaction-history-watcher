830055ffbc32b0565cecc2c4ac18d1bcb0d1a7d3
anxin <anxin@anxins-MacBook-Pro.local>
2019-01-23 16:22:35 +0800

    token tranasction query

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-getTranctionHistory = require('./transactionHistory');
+historyGetter = require('./transactionHistory');
 
 // paras: walletId, page, offset
 // return: promise txs array
-getTranctionHistory("0x88d6F234b6327b7a9d9d23014EFd6E05b6Dc8943",1,10)
-    .then(console.log)
+historyGetter.getEthTransactionHistory("0x88d6F234b6327b7a9d9d23014EFd6E05b6Dc8943",1,10)
+    .then(txs=>console.log(txs.length))
+    .catch(console.log);
+
+historyGetter.getTokenTransactionHistory("0x88d6F234b6327b7a9d9d23014EFd6E05b6Dc8943",4,10)
+.then(txs=>console.log(txs.length))
     .catch(console.log);
--- src/transactionHistory.js
+++ src/transactionHistory.js
@@ -2,28 +2,15 @@ const _=require('lodash');
 const axios = require('axios');
 const config = require('../etc/contractConfig');
 const contractAddress = config.contractAddress;
+const maxIteration = 100000;
 
-module.exports = async function getTranctionHistory(walletId, page, offset) {
-    const host = config.apiHost;
-    let queryParas = {
-        module: "account",
-        action: "txlist",
-        address: walletId,
-        startblock: 0,
-        endblock: 99999999,
-        page: page,
-        offset: offset, // transactions per page
-        sort: "desc",
-        apikey: config.apiKey,
-    };
-    const paras = _.chain(queryParas)
-        .reduce((paras,v,k)=>{
-            paras.push(`${k}=${v}`);
-            return paras;
-        },[])
-        .value()
-        .join('&');
-    const uri = host + '?' + paras;
+module.exports = {
+    getEthTransactionHistory: getEthTransactionHistory,
+    getTokenTransactionHistory: getTokenTransactionHistory,
+};
+
+async function getEthTransactionHistory(walletId, page, offset) {
+    const uri = getUri(walletId,page,offset);
     try {
         const res = await axios.get(uri);
         return _.chain(res.data.result).map(tx=>{
@@ -43,10 +30,57 @@ module.exports = async function getTranctionHistory(walletId, page, offset) {
     } catch(e) {
         throw e
     }
-};
+}
+
+async function getTokenTransactionHistory(walletId, page, offset) {
+    let start = (page-1) * offset;
+    let end = page*offset;
+    let count = 0;
+    let result = [];
+    for(let i =1; i<maxIteration; ++i) {
+        try {
+            let uri = getUri(walletId,i,offset);
+            const res = await axios.get(uri);
+            console.log("data length: "+res.data.result.length);
+            if(res.data.result.length === 0) {
+                break;
+            }
+            _.forEach(res.data.result,(tx=>{
+                if(isSlot(tx) && count>=start) {
+                    result.push({
+                        date: formatBlockTimestamp(tx.timeStamp),
+                        hash: tx.hash,
+                        from: tx.from,
+                        to: tx.to,
+                        value: tx.value/10**18,
+                        isError: tx.isError!=='0',
+                        gasFee: eval(tx.cumulativeGasUsed)*eval(tx.gasPrice)/10**18,
+                        isContractCreation:  isContractCreation(tx),
+                        contractAddress: tx.contractAddress,
+                        isSlot: isSlot(tx),
+                    });
+                    count+=1;
+                }
+                if(count === end) {
+                    return false;
+                }
+            }))
+            if(count === end) {
+                break;
+            }
+        } catch(e) {
+            throw e
+        }
+    }
+    return result;
+}
 
 function isSlot(tx) {
-    return tx.to === contractAddress || tx.from === contractAddress;
+    return tx.to === contractAddress || tx.from === contractAddress || tx.contractAddress === contractAddress;
+}
+
+function isTokenRelated(tx, tokenAddress) {
+    return tx.to === tokenAddress || tx.from === tokenAddress || tx.contractAddress === tokenAddress;
 }
 
 function isContractCreation(tx) {
@@ -68,3 +102,24 @@ function timePadding(time, digit){
     return (Array(digit).fill(0).join('') + time).slice(-digit);
 }
 
+function getUri(walletId, page, offset) {
+    let queryParas = {
+        module: "account",
+        action: "txlist",
+        address: walletId,
+        startblock: 0,
+        endblock: 99999999,
+        page: page,
+        offset: offset, // transactions per page
+        sort: "desc",
+        apikey: config.apiKey,
+    };
+    const paras = _.chain(queryParas)
+        .reduce((paras,v,k)=>{
+            paras.push(`${k}=${v}`);
+            return paras;
+        },[])
+        .value()
+        .join('&');
+    return config.apiHost + '?' + paras;
+}
